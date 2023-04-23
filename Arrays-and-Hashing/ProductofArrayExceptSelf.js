952b9a497ff1fdb47234098d5f3cfaeadd46e3c6
agcast01 <agcast01@gmail.com>
2023-04-23 13:39:04 -0400

    ArrayxSelf

---
+++ Arrays-and-Hashing/ProductofArrayExceptSelf.js
@@ -0,0 +1,15 @@
+var productExceptSelf = function(nums) {
+    const answer = Array(nums.length)
+    let prefix = 1
+    answer.fill(1)
+    for(let i = 0; i < nums.length; i++){
+        answer[i] = prefix;
+        prefix *= nums[i]
+    }
+    let postfix = 1;
+    for(let i = nums.length - 1; i >= 0; i--) {
+        answer[i] *= postfix;
+        postfix *= nums[i]
+    }
+    return answer
+};
