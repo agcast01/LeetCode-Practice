e521f08f28c1b55d98114f3d9cf06d81da63cee4
agcast01 <agcast01@gmail.com>
2023-06-18 11:52:56 -0400

    Next Greater Element

---
+++ Arrays-and-Hashing/nextGreaterElementI.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        # O (n + m)
+        nums1Idx = { n:i for i, n in enumerate(nums1) }
+        res = [-1] * len(nums1)
+
+        stack = []
+        for i in range(len(nums2)):
+            cur = nums2[i]
+
+            # while stack exists and current is greater than the top of the stack
+            while stack and cur > stack[-1]:
+                val = stack.pop() # take top val
+                idx = nums1Idx[val]
+                res[idx] = cur
+
+            if cur in nums1Idx:
+                stack.append(cur)
+        
+        return res
